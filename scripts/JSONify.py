f8042fedc5101f3f298c26dd2cbcd943bd238085
Peter Lu <peterlu94@gmail.com>
2016-07-01 22:40:45 -0400

    Finished csv=>json function for disorders

--- scripts/JSONify.py
+++ scripts/JSONify.py
@@ -3,6 +3,8 @@
 
 import json
 import sys
+import csv
+from classes import KeyWord
 
 # Disorders => JSON <= CSV
 
@@ -10,13 +12,26 @@ def _disorders_to_json(disorder_list, out_file):
 
 	# Taken from http://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file-in-python
 	results = {}
-	disorders_count = len(disorder_list)
 	results['disorders'] = disorder_list
-	results['count'] = disorders_count
+	results['count'] = len(disorder_list)
 
 	json.dump(results, out_file, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-def _csv_to_json(csv_file, out_file):
+def _disorders_csv_to_json(csv_file_name, out_file):
+
+	results = {}
+	disorders_reader = csv.DictReader(open(csv_file_name))
+	disorders_list = [KeyWord(name=row['Keyword'], description=row['Description']) for row in disorders_reader]
+	results['disorders'] = disorders_list
+	results['count'] = len(disorders_list)
+
+	json.dump(results, out_file, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+def _resources_to_json():
+	
+	pass
+
+def _resources_csv_to_json():
 
 	pass
 
@@ -25,6 +40,7 @@ def main(out_file):
 	pass
 
 if __name__ == '__main__':
-	out_file_name = sys.argv[1]
-	out_file = open(out_file_name, 'w')
-	main(out_file)
+	csv_file_name = sys.argv[1]
+	out_file_name = sys.argv[2]
+	with open(out_file_name, 'w') as out_file:
+		_disorders_csv_to_json(csv_file_name, out_file)
