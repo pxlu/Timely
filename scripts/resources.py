8530e9945ed052edfb11ab4b94f33756e01903ed
Peter Lu <peterlu94@gmail.com>
2016-07-02 20:05:51 -0400

    Added stub for resources.py

--- scripts/resources.py
+++ scripts/resources.py
@@ -2,12 +2,21 @@
 # resources.py
 
 # Native libraries
-
-
+import sys
+import json
 # Custom libraries
+from screen import _get_profile
 import common
 import classes
 import JSONify
 
 RESOUCES = common._init_resources_list()
 
+def _get_resources():
+
+	pass
+
+if __name__ == '__main__':
+	in_file_name = sys.argv[1]
+	with open(in_file_name, 'r') as in_file:
+		_get_resources(in_file)
