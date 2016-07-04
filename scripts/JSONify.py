45f7b41d681b6c3ed174cd8fbe55a50bc7675634
Peter Lu <peterlu94@gmail.com>
2016-07-04 14:45:11 -0400

    Finished functions for resources

--- scripts/JSONify.py
+++ scripts/JSONify.py
@@ -43,13 +43,20 @@ def _keywords_csv_to_json(csv_file_name, out_file):
 
 # Resources => JSON <= CSV
 
-def _resources_to_json():
+def _resources_to_json(resources_list, out_file):
 	
-	pass
+	results = {}
+	results['resources'] = resources_list
+	results['count'] = len(resources_list)
 
-def _resources_csv_to_json():
+	json.dump(results, out_file, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-	pass
+def _resources_csv_to_json(csv_file_name, out_file):
+
+	resources_reader = csv.DictReader(open(csv_file_name))
+	resources_list = [Resource(name=row['name'], rID=row['id'], type=row['type'],capacity=row['capacity'], location=row['location'], address=row['address'] if row['address'] != '' else None, contact=row['contact'], services=row['services'], cost=row['cost'] if row['cost'] != ''else None, availibility=None) for row in resources_reader]
+
+	_resources_to_json(resources_list, out_file)
 
 if __name__ == '__main__':
 	csv_file_name = sys.argv[1]
